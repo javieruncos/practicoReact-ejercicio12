c5c988a6a9f5ecd1637c1684b610793cfb2adaec
Javier Uncos <javiersrfc30@gmail.com>
2022-10-09 14:36:44 -0300

    creamos las variables para cambiar categorias en la url

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,35 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import ListaNoticia from "./ListaNoticia";
 
+
+
+
+
+
 const Formulario = () => {
+  let categoria ;
+  let paises;
+  let respuesta;
+
+  const [select, setSelect] = useState("");
+  const [pais, setPais] = useState("")
+  const [arregloNoticia, setarregloNoticia] = useState([]);
    
+  useEffect(() => {
+    consultarApi();
+  },[select]);
 
-    const consultarApi = async()=>{
-        try {
-            const respuesta = await fetch("");
-            const dato = await respuesta.json()
-        } catch (error) {
-            
-        }
+
+  if(select === "Deportes"){
+    categoria= "sports"
+  }else if(select==="Business"){
+     categoria = "business"
+  }else if (select==="Entretenimiento"){
+    categoria="entertainment"
+  }
+
+
+  if(pais === "Argentina"){
+      paises = "ar"
+  }else if(pais === "USA"){
+     paises = "us"
+  }else if(pais === "España"){
+    paises = "es"
+  }
+
+  const consultarApi = async () => {
+    try {
+      respuesta = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=${paises}&category=${categoria}&apiKey=1abe48db8e0d43459467502a3cb538f7`
+      );
+      let dato = await respuesta.json();
+      setarregloNoticia(dato.articles);
+      console.log(dato.articles);
+    } catch (error) {
+      console.log(error);
     }
-    
 
+   
+  };
 
   return (
     <div className="mt-5">
       <Form>
         <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
           <Form.Label className="me-4 mt-2">Selcciona una categoria</Form.Label>
-          <Form.Select aria-label="Default select example" className="w-75">
-            <option>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+          <Form.Select
+            aria-label="Default select example"
+            className="w-50"
+            onChange={(e) => setSelect(e.target.value)}
+          >
+            <option>Selecciona una categoria</option>
+            <option value="Deportes">Deportes</option>
+            <option value="Business">Negocios</option>
+            <option value="Entretenimiento">Entretenimiento</option>
+          </Form.Select>
+          <Form.Select 
+          aria-label="Default select example"
+           className="w-50"
+            onChange={(e)=>setPais(e.target.value)}>
+            <option>Selecciona un pais</option>
+            <option value="Argentina">Argentina</option>
+            <option value="USA">Estados Unidos</option>
+            <option value="España">España</option>
           </Form.Select>
         </Form.Group>
       </Form>
-      <ListaNoticia/>
+      <ListaNoticia arregloNoticia={arregloNoticia} />
     </div>
   );
 };
--- src/components/ListaNoticia.jsx
+++ src/components/ListaNoticia.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Row } from 'react-bootstrap';
 import Noticia from './Noticia';
 
-const ListaNoticia = () => {
+const ListaNoticia = ({arregloNoticia}) => {
     return (
         <div className='mt-5'>
             <Row>
-                <Noticia/>
+
+                {
+                    arregloNoticia.map((item,indice)=><Noticia item={item} key={indice}/>)
+                }
+                
             </Row>
         </div>
     );
